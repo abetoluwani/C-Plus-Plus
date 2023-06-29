b0ec07cb1cdbbff78d5d0ae609037ad22dd184d7
abetoluwani <101352514+cybertolu@users.noreply.github.com>
2023-06-29 17:55:35 +0100

    on colos

--- function.cpp
+++ function.cpp
@@ -2,10 +2,17 @@
 using namespace std;
 
 void happy(string name,int age); // function prototypes
+string concatstr(string str1, string str2);
+
 
 int main(){
     string name = "Raymond";
     int age = 21;
+
+    string name1 = "Hey ";
+    string nal = "Deniz \n";
+    
+    cout << concatstr( name1,nal);
 
     happy(name,age);
 
@@ -20,4 +27,9 @@ void happy(string name, int age){
     cout << "Happy Birthday To " << name <<"\n";
     cout << "You are " << age << "years old \n";
     
+}
+
+
+string concatstr(string str1, string str2){
+    return str1+str2;
 }
