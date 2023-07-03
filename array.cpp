2d592f1c8c3c11a67fe181aa4312c6a572d95771
abetoluwani <101352514+cybertolu@users.noreply.github.com>
2023-07-04 00:37:33 +0100

    on colos

--- array.cpp
+++ array.cpp
@@ -2,3 +2,19 @@
 #include <ctime>
 
 using namespace std;
+
+int main(){
+
+    // arrays can be of the same data type
+
+    string cars []= {"Lambo","Mustang","C50","Bmw","Camry"};
+    double price [] = {250000,75000,15000,20000,5000};
+
+    cout << "car: " << cars [0] << " Price: "<< price [0] <<"\n";
+    cout << "car: " << cars [1] << " Price: "<< price [1] <<"\n";
+    cout << "car: " << cars [2] << " Price: "<< price [2] <<"\n";
+    cout << "car: " << cars [3] << " Price: "<< price [3] <<"\n";
+    cout << "car: " << cars [4] << " Price: "<< price [4] <<"\n";
+
+    return 0;
+}
